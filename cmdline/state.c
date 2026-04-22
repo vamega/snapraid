f0c12359f9a1e0a3916a0d10a554c8fb49a93583
Andrea Mazzoleni <amadvance@gmail.com>
2026-04-22 19:58:08 +0200

    Get the UUID from the imported file or directory

--- cmdline/state.c
+++ cmdline/state.c
@@ -676,7 +676,6 @@ void state_config(struct snapraid_state* state, const char* path, const char* co
 			char device[PATH_MAX];
 			char* split_map[SPLIT_MAX + 1];
 			unsigned split_mac;
-			char* slash;
 			uint64_t dev;
 			int skip_access;
 
@@ -722,17 +721,15 @@ void state_config(struct snapraid_state* state, const char* path, const char* co
 
 					/* get the device of the directory containing the parity file */
 					pathimport(device, sizeof(device), split_map[s]);
-					slash = strrchr(device, '/');
-					if (slash)
-						*slash = 0;
-					else
-						pathcpy(device, sizeof(device), ".");
+
+					/* use only the dir, as the parity file may not exist yet */
+					pathcut(device);
 
 					if (stat(device, &st) == 0) {
 						dev = st.st_dev;
 
 						/* read the uuid, if unsupported use an empty one */
-						if (devuuid(dev, split_map[s], uuid, sizeof(uuid)) != 0) {
+						if (devuuid(dev, device, uuid, sizeof(uuid)) != 0) {
 							*uuid = 0;
 						}
 					} else {
@@ -970,7 +967,7 @@ void state_config(struct snapraid_state* state, const char* path, const char* co
 					dev = st.st_dev;
 
 					/* read the uuid, if unsupported use an empty one */
-					if (devuuid(dev, dir, uuid, sizeof(uuid)) != 0) {
+					if (devuuid(dev, device, uuid, sizeof(uuid)) != 0) {
 						*uuid = 0;
 					}
 
@@ -1071,7 +1068,7 @@ void state_config(struct snapraid_state* state, const char* path, const char* co
 					dev = st.st_dev;
 
 					/* read the uuid, if unsupported use an empty one */
-					if (devuuid(dev, dir, uuid, sizeof(uuid)) != 0) {
+					if (devuuid(dev, device, uuid, sizeof(uuid)) != 0) {
 						*uuid = 0;
 					}
 
