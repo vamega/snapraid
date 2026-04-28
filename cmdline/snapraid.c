8dd7a5e3211cd6a61a109f8abe317bd8346beb23
Andrea Mazzoleni <amadvance@gmail.com>
2026-04-28 11:51:59 +0200

    Add regression check for Btrfs
    It requires root privileges as it has to create Btrfs filesystems.

--- cmdline/snapraid.c
+++ cmdline/snapraid.c
@@ -691,6 +691,7 @@ void config(char* conf, size_t conf_size, const char* argv0)
 #define OPT_TEST_SPEED_PERIOD 307
 #define OPT_TEST_SPEED_DISKS_NUMBER 308
 #define OPT_TEST_SPEED_BLOCKS_SIZE 309
+#define OPT_TEST_KILL_BEFORE_SYNC 310
 
 
 #if HAVE_GETOPT_LONG
@@ -745,6 +746,9 @@ static struct option long_options[] = {
 
 	/* After syncing, do not write the new content file */
 	{ "test-kill-after-sync", 0, 0, OPT_TEST_KILL_AFTER_SYNC },
+
+	/* Do not execure syncing after writing the new initial content file */
+	{ "test-kill-before-sync", 0, 0, OPT_TEST_KILL_BEFORE_SYNC },
 
 	/* Exit with failure if after check/fix there ARE NOT unrecoverable errors. */
 	{ "test-expect-unrecoverable", 0, 0, OPT_TEST_EXPECT_UNRECOVERABLE },
@@ -1292,6 +1296,9 @@ int snapraid_main(int argc, char* argv[])
 			break;
 		case OPT_TEST_KILL_AFTER_SYNC :
 			opt.kill_after_sync = 1;
+			break;
+		case OPT_TEST_KILL_BEFORE_SYNC :
+			opt.kill_before_sync = 1;
 			break;
 		case OPT_TEST_EXPECT_UNRECOVERABLE :
 			opt.expect_unrecoverable = 1;
